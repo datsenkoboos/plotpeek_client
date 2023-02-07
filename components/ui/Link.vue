d3fc4f5f3bf89bc81d1702aca8e85ad5405aedff
datsenkoboos <adacenkoboos@gmail.com>
2023-02-07 21:41:34 +0700

    cant truncate

--- components/ui/Link.vue
+++ components/ui/Link.vue
@@ -1,5 +1,8 @@
 <template>
-  <nuxt-link to="/" class="font-medium flex gap-2 items-center">
+  <nuxt-link
+    to="/"
+    class="font-medium flex gap-2 items-center"
+  >
     <slot />
   </nuxt-link>
 </template>
--- components/ui/Plotpeek.vue
+++ components/ui/Plotpeek.vue
@@ -1,12 +1,19 @@
 <template>
-  <nuxt-link :to="`/plotpeek/${data.id}`" class="w-full border-[1px] rounded-lg">
-    <div class="flex items-end gap-2 py-2 px-4 brand-gradient w-full rounded-t-lg text-white font-medium">
-
-        <span class="text-2xl">{{  data.name  }}</span>
-        <span class="text-xs pb-1">{{  data.author  }}</span>
-        
+  <nuxt-link
+    :to="`/plotpeek/${data.id}`"
+    class="w-full border-[1px] rounded-lg"
+  >
+    <div class="flex gap-5 py-3 px-5 w-full rounded-t-lg font-medium">
+      <div class="max-w-[78px]">
+        <h1 class="text-2xl text-brand-gradient">{{ data.name }}</h1>
+        <UiLink class="text-sm gap-1 text-ellipsis overflow-hidden whitespace-nowrap" :to="`/search?q=${data.author}`">
+            {{ data.author }}sfdasdfasdf
+        </UiLink>
+      </div>
+      <div class="text-xs flex items-end text-left">
+          {{ data.description }}
+      </div>
     </div>
-    
   </nuxt-link>
 </template>
 <script setup lang="ts">
